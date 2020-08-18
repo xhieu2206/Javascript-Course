1e5a8701f8e9f8615e9e3aea3acfeed89960fd26
xhieu2206 <xhieu94@gmail.com>
2020-08-18 22:31:32 +0700

    S7 Let & Const

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,48 @@
+// Lecture: Let & Const
+
+// ES5
+/*
+var nameE = 'Hieu Nguyen';
+var age5 = 26;
+nameE = 'Hieu Xuan';
+console.log(nameE);
+*/
+
+// ES6
+/*
+const name6 = 'Hieu Nguyen';
+let age6 = 26;
+name6 = 'Hieu Xuan';
+console.log(name6);
+*/
+
+// ES5
+/*
+function driversLicense(passedTesst) {
+  if (passedTesst) {
+    var firstName = 'Hieu';
+    var yob = 1994;
+
+    console.log(firstName + ' sinh vào năm ' + yob + ' đã được phép lái xe');
+  }
+}
+
+driversLicense(true);
+*/
+
+// ES6
+/*
+function driversLicense6(passedTesst) {
+  let firstName;
+  const yob = 1994;
+
+  if (passedTesst) {
+    firstName = 'Hieu';
+  }
+
+  console.log(firstName + ' sinh vào năm ' + yob + ' đã được phép lái xe');
+}
+
+driversLicense6(true);
+*/
+
