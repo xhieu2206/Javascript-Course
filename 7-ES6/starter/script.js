670cfe40c63bb7644866a52e82cb30af24ac59bf
xhieu2206 <xhieu94@gmail.com>
2020-08-20 02:51:31 +0700

    S7: Arrow Function: about this keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -60,6 +60,7 @@ console.log(c);
 // Lecture: Strings
 
 // Arrow Function: Basics
+/*
 const years = [1994, 2000, 2003, 1996];
 
 // ES5
@@ -77,3 +78,84 @@ ages6 = years.map((year, index) => {
   const age = now - year;
   return `Age element ${index++}: ${age}`;
 });
+*/
+
+// Arrow Function: Lexical 'this' keyword
+
+// ES5
+/*
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var _self = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'đây là hộp thứ ' + _self.position + ' có màu ' + _self.color;
+      alert(str);
+    });
+  }
+}
+
+box5.clickMe();
+*/
+
+// ES6
+/*
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'đây là hộp thứ ' + this.position + ' có màu ' + this.color;
+      alert(str);
+    });
+  }
+}
+
+box6.clickMe(); // đây là hộp thứ 1 có màu green
+*/
+
+/*
+const box66 = {
+  color: 'green',
+  position: 1,
+  clickMe: () => {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'đây là hộp thứ ' + this.position + ' có màu ' + this.color;
+      alert(str);
+    });
+  }
+}
+
+box66.clickMe(); // đây là hộp thứ undefined có màu undefined
+*/
+
+function Person(name) {
+  this.name = name;
+}
+
+/*
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(function(name) {
+    return this.name + ' là bạn của ' + name;
+  }.bind(this)); // ở đây chúng ta sẽ bind function(name) { ... } với object là object tương ứng với THIS keyword, THIS keyword ở đây được xác định là object được tạo bởi function constructor Person.
+
+  console.log(arr);
+}
+
+var friends = ['Bob', 'Mary', 'John'];
+var hieu = new Person('Hieu');
+hieu.myFriends5(friends);  // ["Hieu là bạn của Bob", "Hieu là bạn của Mary", "Hieu là bạn của John"]
+*/
+
+// Person.prototype.myFriends6 = function(friends) {
+//   var arr = friends.map((name) => {
+//     return this.name + ' là bạn của ' + name;
+//   });
+
+//   console.log(arr);
+// }
+
+// var friends = ['Bob', 'Mary', 'John'];
+// var hieu = new Person('Hieu');
+// hieu.myFriends6(friends);  // ["Hieu là bạn của Bob", "Hieu là bạn của Mary", "Hieu là bạn của John"]
