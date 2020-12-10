e42d924a65aeb7ee0ea27b12b8f64712eddc6a61
xhieu2206 <xhieu94@gmail.com>
2020-12-11 01:36:49 +0700

    Done Section 7 ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -456,6 +456,8 @@ Person6.greeting();
 
 //////////////////////////////////////////////
 // Lecture: Classes with sub-classes
+
+/*
 var Person5 = function(name, yob, job) {
   this.name = name;
   this.yob = yob;
@@ -509,3 +511,86 @@ class Athlete6 extends Person6 {
 const hieu6 = new Athlete6('Hieu Six', 1994, 'table tennis player', 100, 10);
 hieu6.calcAge();
 hieu6.wonMedal();
+*/
+
+//////////////////////////////////////////////
+// Lecture: Coding Challenge
+
+// My Solution
+(function() {
+  let calcAverageAndTotal = (arr) => {
+    return [arr.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0) / arr.length, arr.reduce((acc, cur) => acc + cur, 0)];
+  }
+  class Item {
+    constructor(name, buildYear) {
+      this.name = name;
+      this.buildYear = buildYear;
+    }
+  }
+
+  class Park extends Item {
+    constructor(name, buildYear, numberOfTree, parkArea) {
+      super(name, buildYear);
+      this.numberOfTree = numberOfTree;
+      this.parkArea = parkArea;
+    }
+
+    calcAge() {
+      return new Date().getFullYear() - this.buildYear;
+    }
+
+    calcTreesDensity() {
+      return this.numberOfTree / this.parkArea;
+    }
+  }
+
+  class Street extends Item {
+    constructor(name, buildYear, length, size = 'Normal') {
+      super(name, buildYear);
+      this.length = length;
+      this.size = size;
+    }
+  }
+
+  let parks = [
+    new Park('Park 1', 2000, 1000, 1000),
+    new Park('Park 2', 1994, 500, 2000),
+    new Park('Park 3', 2002, 1500, 500),
+    new Park('Park 4', 1999, 999, 800),
+    new Park('Park 5', 2017, 1010, 1001)
+  ]
+
+  let streets = [
+    new Street('Street 1', 2000, 2040),
+    new Street('Street 2', 1994, 1000, 'Tiny'),
+    new Street('Street 3', 2002, 2000),
+    new Street('Street 4', 1999, 2100, 'Small'),
+    new Street('Street 5', 2017, 920, 'Big'),
+    new Street('Street 6', 1924, 1500, 'Huge')
+  ]
+
+  console.log('---- PARKS REPORT -----');
+  console.log(`${parks.length} công viên có tuổi trung bình là ${calcAverageAndTotal(
+    parks.map(park => park.calcAge())
+  )[0]}`);
+  parks.forEach(cur => {
+    console.log(`Công viên ${cur.name} có mật độ cây là ${cur.calcTreesDensity()} cây / km2`);
+  });
+  parks.forEach(cur => {
+    if (cur.numberOfTree >= 1000) {
+      console.log(`Công viên ${cur.name} có nhiều hơn 1000 cây`);
+    }
+  });
+
+  console.log('---- STREETS REPORT -----');
+  console.log(`${streets.length} phố có tổng độ dài là ${calcAverageAndTotal(
+    streets.map(cur => cur.length)
+  )[1]} km, trung bình là ${calcAverageAndTotal(
+    streets.map(cur => cur.length)
+  )[0]} km`);
+  streets.forEach(cur => {
+    console.log(`Size của phố ${cur.name} là ${cur.size}`);
+  });
+})();
