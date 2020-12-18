3ae9fdb0931927b7bd11183cfccec208a82de376
xhieu2206 <xhieu94@gmail.com>
2020-12-19 01:25:33 +0700

    Section 9: done video 18

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from './views/base';
+import * as searchView from './views/searchView';
 
 /** Global sate of app
  * - Search object
@@ -11,23 +13,27 @@ const state = {} // init một empty object
 const controlSearch = async () => { // do chúng ta sẽ dùng một await promise, ở đây là promise getResults, do đó chúng ta sẽ "nói trước" đây là một async function
 
   // 1. get the query from the view
-  const query = 'Pizza'; // TODO, tạm thời hardcode
+  const query = searchView.getInput(); // TODO, tạm thời hardcode
 
   if (query) {
     // 2. new search object and add to state
     state.search = new Search(query);
 
     // 3. prepare UI for results
+    searchView.clearInput(); // clear input
+    searchView.clearResults(); // xóa các kết quả cũ trước khi thêm vào các recipes mới.
+    renderLoader(elements.searchRes);
 
     // 4. Search for recipes
     await state.search.getResults(); // return về một promise do đây là async function (method của Search instance)
 
     // 5. render the results to UI
-    console.log(state.search.result);
+    clearLoader(); // remove the loader spinner trước khi hiển thị dữ liệu
+    searchView.renderResults(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault(); // ngăn trang web load lại
   controlSearch();
 });
