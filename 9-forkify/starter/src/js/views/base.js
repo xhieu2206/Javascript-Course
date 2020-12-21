44fd8165a5b3a8edc205ff31bf9fa1886e139e0a
xhieu2206 <xhieu94@gmail.com>
2020-12-21 23:53:18 +0700

    Section 9: done video 27

--- 9-forkify/starter/src/js/views/base.js
+++ 9-forkify/starter/src/js/views/base.js
@@ -5,7 +5,8 @@ export const elements = {
   searchResultList: document.querySelector('.results__list'),
   searchResPages: document.querySelector('.results__pages'),
   searchResPages: document.querySelector('.results__pages'),
-  recipe: document.querySelector('.recipe')
+  recipe: document.querySelector('.recipe'),
+  shopping: document.querySelector('.shopping__list')
 }
 
 export const elementStrings = {
